a549130672d131ab1245fcab95d425e603fb87d6
Rohit Nagraj <rohitnagraj.99@gmail.com>
2025-09-23 17:00:41 -0400

    Added dev scripts

---
+++ scripts-dev/download_models.sh
@@ -0,0 +1,16 @@
+export HF_HOME="/data/"
+huggingface-cli download deepseek-ai/DeepSeek-V3-0324
+# huggingface-cli download meta-llama/Llama-3.1-8B-Instruct
+# huggingface-cli download mistralai/Mistral-7B-Instruct-v0.3
+# huggingface-cli download deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct
+# huggingface-cli download meta-llama/Llama-3.1-70B-Instruct
+# huggingface-cli download mistralai/Mixtral-8x7B-Instruct-v0.1
+# huggingface-cli download Qwen/Qwen2-57B-A14B-Instruct
+# huggingface-cli download neuralmagic/Meta-Llama-3.1-8B-Instruct-FP8
+# huggingface-cli download neuralmagic/Mistral-7B-Instruct-v0.3-FP8
+# huggingface-cli download neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8
+# huggingface-cli download neuralmagic/Qwen2-72B-Instruct-FP8
+# huggingface-cli download neuralmagic/Qwen2-57B-A14B-Instruct-FP8
+# huggingface-cli download neuralmagic/Mixtral-8x7B-Instruct-v0.1-FP8
+# huggingface-cli download google/gemma-2-27b-it
+# huggingface-cli download neuralmagic/DeepSeek-Coder-V2-Lite-Instruct-FP8
