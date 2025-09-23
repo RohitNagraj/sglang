7e370eca9f14e1c47ac694e8e05bc878221e4490
Rohit Nagraj <rohitnagraj.99@gmail.com>
2025-09-23 18:17:25 -0400

    Updated dev scripts

--- scripts-dev/test_package.sh
+++ scripts-dev/test_package.sh
@@ -12,28 +12,28 @@ uv venv p313 -p 3.13
 
 source /home/rpujarna/p310/bin/activate
 # uv pip install -i https://test.pypi.org/simple/ sgl-kernel-rpujarna-test==0.3.10
-uv pip install /home/rpujarna/Work/dev/sglang/sgl-kernel/dist/sgl_kernel_rpujarna_test-0.3.10-cp310-abi3-manylinux2014_x86_64.whl
+uv pip install /home/rpujarna/Work/dev/sglang/sgl-kernel/dist/sgl_kernel-0.3.10-cp310-abi3-manylinux2014_x86_64.whl
 uv pip install numpy
 uv pip install torch --index-url https://download.pytorch.org/whl/rocm6.4
 deactivate
 
 source /home/rpujarna/p311/bin/activate
 # uv pip install -i https://test.pypi.org/simple/ sgl-kernel-rpujarna-test==0.3.10
-uv pip install /home/rpujarna/Work/dev/sglang/sgl-kernel/dist/sgl_kernel_rpujarna_test-0.3.10-cp310-abi3-manylinux2014_x86_64.whl
+uv pip install /home/rpujarna/Work/dev/sglang/sgl-kernel/dist/sgl_kernel-0.3.10-cp310-abi3-manylinux2014_x86_64.whl
 uv pip install numpy
 uv pip install torch --index-url https://download.pytorch.org/whl/rocm6.4
 deactivate
 
 source /home/rpujarna/p312/bin/activate
 # uv pip install -i https://test.pypi.org/simple/ sgl-kernel-rpujarna-test==0.3.10
-uv pip install /home/rpujarna/Work/dev/sglang/sgl-kernel/dist/sgl_kernel_rpujarna_test-0.3.10-cp310-abi3-manylinux2014_x86_64.whl
+uv pip install /home/rpujarna/Work/dev/sglang/sgl-kernel/dist/sgl_kernel-0.3.10-cp310-abi3-manylinux2014_x86_64.whl
 uv pip install numpy
 uv pip install torch --index-url https://download.pytorch.org/whl/rocm6.4
 deactivate
 
 source /home/rpujarna/p313/bin/activate
 # uv pip install -i https://test.pypi.org/simple/ sgl-kernel-rpujarna-test==0.3.10
-uv pip install /home/rpujarna/Work/dev/sglang/sgl-kernel/dist/sgl_kernel_rpujarna_test-0.3.10-cp310-abi3-manylinux2014_x86_64.whl
+uv pip install /home/rpujarna/Work/dev/sglang/sgl-kernel/dist/sgl_kernel-0.3.10-cp310-abi3-manylinux2014_x86_64.whl
 uv pip install numpy
 uv pip install torch --index-url https://download.pytorch.org/whl/rocm6.4
 deactivate
