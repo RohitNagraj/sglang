c442214c29ea8c1c099ff796f87f2dab3c6954da
Rohit Nagraj <rohitnagraj.99@gmail.com>
2025-09-23 21:42:54 -0500

    Cleaned branch

--- scripts-dev/run_sgl_kernel_tests.sh
+++
@@ -1,5 +0,0 @@
-# This runs the unit-test-sgl-kernel-amd from .github/workflows/pr-test-amd.yml
-
-bash scripts/ci/amd_ci_start_container.sh
-bash scripts/ci/amd_ci_install_dependency.sh
-docker exec -w /sglang-checkout/sgl-kernel/tests ci_sglang python3 -m pytest test_moe_align.py test_moe_topk_softmax.py test_apply_token_bitmask_inplace.py test_activation.py test_kvcacheio.py speculative/test_eagle_utils.py
--- scripts-dev/run_sgl_performance_tests.sh
+++
@@ -1,5 +0,0 @@
-HF_HOME="/data/"
-cd /Work/dev/sglang/test/srt
-# SGLANG_AMD_CI=1 SGLANG_IS_IN_CI=1 SGLANG_USE_AITER=1 python3 run_suite.py --suite per-commit-8-gpu-amd 2>&1 | tee per-commit-8-gpu-amd.txt
-# SGLANG_AMD_CI=1 SGLANG_IS_IN_CI=1 SGLANG_USE_AITER=1 python3 run_suite.py --suite nightly-amd 2>&1 | tee nightly-amd.txt
-SGLANG_AMD_CI=1 SGLANG_IS_IN_CI=1 SGLANG_USE_AITER=1 python3 run_suite.py --suite per-commit-amd  --range-begin 46 --range-end 47
